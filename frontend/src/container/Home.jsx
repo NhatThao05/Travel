6e0087d9f853645640b452446ccfed23b2c7bada
Thao <levunhatthao05@gmail.com>
2022-08-23 20:49:54 +0300

    Navbar

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,7 +1,47 @@
-import React from "react"
+import React, {useState, useEffect, useRef} from "react";
+import {HiMenu} from 'react-icons/hi';
+import {AiFillCloseCircle} from 'react-icons/ai';
+import { Link, Route, Routes } from "react-router-dom";
+
+import Navbar from "../components/Navbar";
+import UserProfile from "../components/UserProfile";
+import { client } from "../client";
+import Logo from '../assets/Logo.png'
+import Pins from "./Pins";
+import { userQuery } from "../utils/data";
 
 export default function Home() {
+ const [toggleNavbar, setToggleNavbar] = useState(false);
+ const [user, setUser] = useState(null);
+ const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+ useEffect(() => {
+  const query = userQuery(userInfo?.googleId);
+  client.fetch(query).then((data) => {
+   setUser(data[0])
+  })
+ }, [])
  return(
-  <h1>Home component</h1>
+  <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+   <div className="hidden md:flex h-screen flex-initial">
+    <Navbar user={user && user} />
+   </div>
+   <div className="flex md:hidden flex-row">
+    <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleNavbar(true)}/>
+    <Link to="/">
+      <img src={Logo} alt="logo" className="w-10"/>
+    </Link>
+    <Link to={`user-profile/${user?._id}`}>
+      <img src="https://findicons.com/files/icons/926/soccer_teams/256/manchester_united.png" alt="logo" className="w-10"/>
+    </Link>
+   </div>
+   {toggleNavbar && (
+    <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+     <div className="absolute w-full flex justify-end items-center p-2">
+      <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleNavbar(false)}/>
+     </div>
+     <Navbar user={user && user} closeToggle={setToggleNavbar}/>
+    </div>
+   )}
+  </div>
  )
 }
---
+++ frontend/src/container/Pins.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Pins() {
+ return(
+  <h1>Pins component</h1>
+ )
+}
