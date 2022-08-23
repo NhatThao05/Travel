4a233d86b7d3c22cf27beec3dfc8696eca4860ca
Thao <levunhatthao05@gmail.com>
2022-08-23 22:30:21 +0300

    navbar complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,58 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import {RiHomeFill} from 'react-icons/ri';
+import {IoArrowForwardCircleOutline} from 'react-icons/io';
+import Logo from '../assets/Logo.png';
 
+export default function Navbar({user, closeToggle}) {
+ const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+ const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
 
-export default function Navbar() {
+ const categories = [
+  {name: 'Vehicles'},
+  {name: 'Fishing'},
+  {name: 'Entertainment'},
+  {name: 'Seafood'},
+  {name: 'Hotel'},
+  {name: 'Others'}
+ ]
+ const handleCloseNavbar = () => {
+  if(closeToggle) closeToggle(false) 
+ }
  return(
-  <h1>Navbar component</h1>
+  <div className="flex flex-col justify-between bg-white h-full overflow-y-scrikk min-w-20 hide-scrollbar">
+   <div className="flex flex-col">
+    <Link to="/" className="flex px-5 gap-2 my-6 pt-1 w-190 items-center" onClick={handleCloseNavbar}>
+     <img src={Logo} alt="logo" className="w-12"/>
+    </Link>
+    <div className="flex flex-col gap-5">
+     <NavLink
+      to="/"
+      className={({isActive}) => (isActive ? isActiveStyle : isNotActiveStyle)}
+      onClick={handleCloseNavbar}
+     >
+       <RiHomeFill color="black"/>
+       Home
+     </NavLink>
+     <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover destination</h3>
+     {categories.slice(0, categories.length - 1).map((category) => (
+      <NavLink to={`/category/${category.name}`}
+        className={({isActive}) => (isActive ? isActiveStyle : isNotActiveStyle)}
+        onClick={handleCloseNavbar} key={category.name}
+      >
+        {category.name}
+      </NavLink>
+     ))}
+   </div>
+   </div>
+   {user && (
+    <Link to={`user-profile/${user._id}`} 
+      className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+      onClick={handleCloseNavbar}>
+     <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile"/>
+     <p>{user.userName}</p>
+    </Link>
+   )}
+  </div>
  )
 }
--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -13,12 +13,19 @@ import { userQuery } from "../utils/data";
 export default function Home() {
  const [toggleNavbar, setToggleNavbar] = useState(false);
  const [user, setUser] = useState(null);
+ const scrollRef = useRef(null);
+
  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
  useEffect(() => {
   const query = userQuery(userInfo?.googleId);
   client.fetch(query).then((data) => {
    setUser(data[0])
   })
+ }, [])
+
+ useEffect(() => {
+  scrollRef.current.scrollTo(0,0)
  }, [])
  return(
   <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
@@ -26,22 +33,31 @@ export default function Home() {
     <Navbar user={user && user} />
    </div>
    <div className="flex md:hidden flex-row">
-    <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleNavbar(true)}/>
-    <Link to="/">
-      <img src={Logo} alt="logo" className="w-10"/>
-    </Link>
-    <Link to={`user-profile/${user?._id}`}>
-      <img src="https://findicons.com/files/icons/926/soccer_teams/256/manchester_united.png" alt="logo" className="w-10"/>
-    </Link>
-   </div>
-   {toggleNavbar && (
-    <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-     <div className="absolute w-full flex justify-end items-center p-2">
-      <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleNavbar(false)}/>
-     </div>
-     <Navbar user={user && user} closeToggle={setToggleNavbar}/>
+    <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+     <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleNavbar(true)}/>
+     <Link to="/">
+       <img src={Logo} alt="logo" className="w-10"/>
+     </Link>
+     <Link to={`user-profile/${user?._id}`}>
+       <img src="https://toppng.com/uploads/preview/travel-icons-travel-icon-free-1155345307059cfuzjxyz.png" alt="logo" className="w-10"/>
+     </Link>
     </div>
-   )}
+    {toggleNavbar && (
+     <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+      <div className="absolute w-full flex justify-end items-center p-2">
+       <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleNavbar(false)}/>
+      </div>
+      <Navbar user={user && user} closeToggle={setToggleNavbar}/>
+     </div>
+    )}
+   </div>
+   
+   <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+     <Routes>
+      <Route path="/user-profile/:userId" element={<UserProfile />} />
+      <Route path="/*" element={<Pins user={user && user} />} />
+     </Routes>
+   </div>
   </div>
  )
 }
